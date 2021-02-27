e74547b60941446c63f0f9669a924714ef45b4bb
nandha_the_ken <nandha@the-ken.com>
2021-02-27 21:55:43 +0530

    Initial commit

---
+++ app/src/main/java/com/example/kotlincompose/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.example.kotlincompose
+
+import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.kotlincompose.ui.theme.KotlinComposeTheme
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            KotlinComposeTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    KotlinComposeTheme {
+        Greeting("Android")
+    }
+}
